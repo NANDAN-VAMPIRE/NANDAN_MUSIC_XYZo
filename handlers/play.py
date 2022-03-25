bd23672656da04956a3546072d4c653f9dc21aca
NANDAN✓ <89117179+NANDAN-VAMPIRE@users.noreply.github.com>
2022-03-25 18:51:12 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -92,7 +92,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
     draw.text(
         (190, 670),
-        f"Powered By: 👑NANDAN_VAMPIRE (@NANDAN_VAMPIRE)",
+        f"Powered By: 👑NANDAN_VAMPIRE (@NANDAN_SINGH_BHUMIHAR)",
         (255, 255, 255),
         font=font,
     )
